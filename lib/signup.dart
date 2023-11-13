ae9745f0e7530ec4aac637d11a3732649a6c8a31
Albertus Simanullang <albertsimanullang12@gmail.com>
2023-11-13 09:49:26 +0700

    penambahan API

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:kren/login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SignupPage extends StatefulWidget {
   @override
@@ -12,50 +13,141 @@ class _SignupPageState extends State<SignupPage> {
   final _conPassword = TextEditingController();
   final _conCPassword = TextEditingController();
 
-  bool _passwordVisible = false;
-  bool _cPasswordVisible = false;
-
   String _usernameNotification = "";
   String _emailNotification = "";
   String _passwdNotification = "";
   String _cpasswdNotification = "";
+  String _selectedProvinceId = "";
+  String _selectedProvince = "";
+  String _selectedRegency = "";
+
+  List<Map<String, dynamic>> _provinces = [];
+  List<String> _regencies = [];
+
+  Future<void> _fetchProvinces() async {
+    try {
+      final response = await http.get(Uri.parse(
+          'https://emsifa.github.io/api-wilayah-indonesia/api/provinces.json'));
+
+      if (response.statusCode == 200) {
+        final List<Map<String, dynamic>> provincesData =
+            List<Map<String, dynamic>>.from(
+          (json.decode(response.body) as List<dynamic>).map((province) {
+            return {
+              "name": province["name"],
+              "id": province["id"],
+            };
+          }),
+        );
+
+        setState(() {
+          _provinces = provincesData;
+        });
+      } else {
+        print('Failed to load provinces');
+      }
+    } catch (error) {
+      print('Error fetching provinces: $error');
+    }
+  }
+
+  Future<void> _fetchRegencies(String provinceId) async {
+    try {
+      final response = await http.get(Uri.parse(
+          'https://emsifa.github.io/api-wilayah-indonesia/api/regencies/$provinceId.json'));
+
+      if (response.statusCode == 200) {
+        final List<String> regenciesData =
+            List<String>.from(json.decode(response.body));
+
+        setState(() {
+          _regencies = regenciesData;
+        });
+      } else {
+        print('Failed to load regencies');
+      }
+    } catch (error) {
+      print('Error fetching regencies: $error');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchProvinces();
+  }
 
   signUp(BuildContext context) {
-    String uname = _conUsername.text;
-    String email = _conEmail.text;
-    String passwd = _conPassword.text;
-    String cpasswd = _conCPassword.text;
-
-    String usernameNotificationText = "";
-    String emailNotificationText = "";
-    String passwdNotificationText = "";
-    String cpasswdNotificationText = "";
-
-    if (uname.isEmpty) {
-      usernameNotificationText = "Masukkan Username Anda!";
-    } else if (email.isEmpty) {
-      emailNotificationText = "Masukkan Email Anda!";
-    } else if (passwd.isEmpty) {
-      passwdNotificationText = "Masukkan Password Anda!";
-    } else if (cpasswd.isEmpty || passwd != cpasswd) {
-      cpasswdNotificationText = "Password Anda Tidak Sesuai!";
-    }
-
-    setState(() {
-      _usernameNotification = usernameNotificationText;
-      _emailNotification = emailNotificationText;
-      _passwdNotification = passwdNotificationText;
-      _cpasswdNotification = cpasswdNotificationText;
-    });
-
-    if (!_passwordVisible) {
-      _conPassword.text = passwd;
-    }
-    if (!_cPasswordVisible) {
-      _conCPassword.text = cpasswd;
-    }
-
     // Add your signup logic here
+  }
+
+  Widget provinceDropdown() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Province',
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            color: Colors.black87,
+          ),
+        ),
+        SizedBox(height: 5),
+        DropdownButton<String>(
+          isExpanded: true,
+          value: _selectedProvince.isNotEmpty ? _selectedProvince : null,
+          items: _provinces.map((province) {
+            return DropdownMenuItem<String>(
+              value: province["name"],
+              child: Text(province["name"]),
+            );
+          }).toList(),
+          onChanged: (String? value) {
+            setState(() {
+              _selectedProvince = value ?? "";
+              _selectedProvinceId = _provinces.firstWhere(
+                  (province) => province["name"] == _selectedProvince)["id"];
+              _fetchRegencies(_selectedProvinceId);
+            });
+          },
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+  }
+
+  Widget regencyDropdown() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Regency',
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            color: Colors.black87,
+          ),
+        ),
+        SizedBox(height: 5),
+        DropdownButton<String>(
+          isExpanded: true,
+          value: _selectedRegency,
+          items: _regencies.map((regency) {
+            return DropdownMenuItem<String>(
+              value: regency,
+              child: Text(regency),
+            );
+          }).toList(),
+          onChanged: (String? value) {
+            setState(() {
+              _selectedRegency = value!;
+            });
+          },
+        ),
+        SizedBox(height: 10),
+      ],
+    );
   }
 
   Widget inputFile({
@@ -70,8 +162,6 @@ class _SignupPageState extends State<SignupPage> {
     Function(bool isVisible)? isPasswordVisible,
     String? notificationText,
   }) {
-    controller ??= TextEditingController();
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -134,9 +224,6 @@ class _SignupPageState extends State<SignupPage> {
 
   @override
   Widget build(BuildContext context) {
-    _passwordVisible = false;
-    _cPasswordVisible = false;
-
     return Scaffold(
       resizeToAvoidBottomInset: true,
       backgroundColor: Colors.white,
@@ -156,116 +243,91 @@ class _SignupPageState extends State<SignupPage> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 40),
           height: MediaQuery.of(context).size.height - 50,
           width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Text(
-                    "Sign up",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "Sign up",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Create an account, It's free ",
-                    style: TextStyle(fontSize: 15, color: Colors.grey[700]),
-                  )
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  inputFile(
-                    label: 'Username',
-                    controller: _conUsername,
-                    icon: const Icon(Icons.person),
-                    notificationText: _usernameNotification,
-                  ),
-                  inputFile(
-                    label: 'Email',
-                    controller: _conEmail,
-                    icon: const Icon(Icons.email),
-                    notificationText: _emailNotification,
-                  ),
-                  inputFile(
-                    label: 'Password',
-                    obscureText: _passwordVisible,
-                    controller: _conPassword,
-                    icon: const Icon(Icons.lock),
-                    isPasswordVisible: (isVisible) {
-                      setState(() {
-                        _passwordVisible = isVisible;
-                      });
-                    },
-                    notificationText: _passwdNotification,
-                  ),
-                  inputFile(
-                    label: 'Confirm Password',
-                    obscureText: _cPasswordVisible,
-                    controller: _conCPassword,
-                    icon: const Icon(Icons.lock_outline),
-                    isPasswordVisible: (isVisible) {
-                      setState(() {
-                        _cPasswordVisible = isVisible;
-                      });
-                    },
-                    notificationText: _cpasswdNotification,
-                  ),
-                ],
-              ),
-              Container(
-                padding: EdgeInsets.only(),
-                decoration: BoxDecoration(),
-                child: MaterialButton(
-                  minWidth: double.infinity,
-                  height: 60,
-                  onPressed: () {
-                    signUp(context);
-                  },
-                  color: Color(0xff0095FF),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: Text(
-                    "Sign up",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 18,
+                    SizedBox(
+                      height: 20,
                     ),
+                    Text(
+                      "Create an account, It's free ",
+                      style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      inputFile(
+                        label: 'Username',
+                        controller: _conUsername,
+                        icon: const Icon(Icons.person),
+                        notificationText: _usernameNotification,
+                      ),
+                      inputFile(
+                        label: 'Email',
+                        controller: _conEmail,
+                        icon: const Icon(Icons.email),
+                        notificationText: _emailNotification,
+                      ),
+                      provinceDropdown(),
+                      regencyDropdown(),
+                      inputFile(
+                        label: 'Password',
+                        obscureText: false,
+                        controller: _conPassword,
+                        icon: const Icon(Icons.lock),
+                        notificationText: _passwdNotification,
+                      ),
+                      inputFile(
+                        label: 'Confirm Password',
+                        obscureText: false,
+                        controller: _conCPassword,
+                        icon: const Icon(Icons.lock_outline),
+                        notificationText: _cpasswdNotification,
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("Already have an account?"),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginPage()),
-                      );
+                Container(
+                  padding: EdgeInsets.only(),
+                  decoration: BoxDecoration(),
+                  child: MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      signUp(context);
                     },
+                    color: Color(0xff0095FF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
                     child: Text(
-                      " Login",
+                      "Sign up",
                       style: TextStyle(
                         fontWeight: FontWeight.w600,
                         fontSize: 18,
-                        color: Colors.blue,
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
