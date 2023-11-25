7d86baaa2c61125bc78ff4349f984fc01f70da51
Dingzone <91923956+Dingzone@users.noreply.github.com>
2023-11-25 11:48:10 +0700

    Update screen3.dart

--- lib/pages/screen3.dart
+++ lib/pages/screen3.dart
@@ -61,111 +61,179 @@ class _LaporkanKejadianState extends State<LaporkanKejadian> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // Jenis bencana
-            DropdownButtonFormField(
-              value: _selectedJenisBencana,
-              items: JenisBencana.values
-                  .map((e) => DropdownMenuItem(
-                value: e,
-                child: Text(e.name),
-              ))
-                  .toList(),
-              onChanged: (value) {
-                setState(() {
-                  _selectedJenisBencana = value as JenisBencana?;
-                });
-              },
-            ),
-            // Nama bencana
-            TextFormField(
-              controller: _namaBencanaController,
-              decoration: InputDecoration(
-                labelText: "Nama Bencana",
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Teks "Laporkan Kejadian" dan "Bencana" dengan gaya yang diinginkan
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Laporkan Kejadian",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Bencana",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.orange,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Nama Bencana tidak boleh kosong';
-                }
-                return null;
-              },
-            ),
-            // Lokasi bencana
-            TextFormField(
-              controller: _lokasiBencanaController,
-              decoration: InputDecoration(
-                labelText: "Lokasi Bencana",
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Lokasi Bencana tidak boleh kosong';
-                }
-                return null;
-              },
-            ),
-            // Keterangan bencana
-            TextFormField(
-              controller: _keteranganBencanaController,
-              decoration: InputDecoration(
-                labelText: "Keterangan Bencana",
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Keterangan Bencana tidak boleh kosong';
-                }
-                return null;
-              },
-            ),
-            // Gambar
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                // Tombol pilih gambar dari kamera
-                TextButton(
-                  onPressed: () async {
-                    // Pilih gambar dari kamera
-                    final ImageSource source = ImageSource.camera;
+              SizedBox(height: 16.0), // Jarak antara teks dan dropdown
 
-                    // Unggah gambar ke server
-                    final XFile? image =
-                    await ImagePicker().pickImage(source: source);
-
-                    if (image != null) {
-                      setState(() {
-                        _image = File(image.path);
-                      });
-                    }
+              // Jenis bencana
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: DropdownButtonFormField(
+                  value: _selectedJenisBencana,
+                  hint: Text("Pilih Jenis Bencana"),
+                  items: JenisBencana.values
+                      .map((e) => DropdownMenuItem(
+                            value: e,
+                            child: Text(e.name),
+                          ))
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedJenisBencana = value as JenisBencana?;
+                    });
                   },
-                  child: Text("Pilih Gambar dari Kamera"),
-                ),
-              ],
-            ),
-            _image != null
-                ? Image.file(
-              _image!,
-              width: 200,
-              height: 200,
-            )
-                : Container(),
-            // Tombol Lapor
-            ElevatedButton(
-              onPressed: () {
-                if (_validateForm()) {
-                  _submitReport();
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue, // Use backgroundColor instead of primary
-              ),
-              child: Text(
-                'Lapor',
-                style: TextStyle(
-                  color: Colors.white, // Warna teks tombol
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 16.0), // Jarak antara dropdown dan textfield
+
+              // Nama bencana
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextFormField(
+                  controller: _namaBencanaController,
+                  decoration: InputDecoration(
+                    labelText: "Nama Bencana",
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nama Bencana tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(height: 16.0), // Jarak antara textfield dan textfield berikutnya
+
+              // Lokasi bencana
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextFormField(
+                  controller: _lokasiBencanaController,
+                  decoration: InputDecoration(
+                    labelText: "Lokasi Bencana",
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Lokasi Bencana tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(height: 16.0), // Jarak antara textfield dan textfield berikutnya
+
+              // Keterangan bencana
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextFormField(
+                  controller: _keteranganBencanaController,
+                  decoration: InputDecoration(
+                    labelText: "Keterangan Bencana",
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Keterangan Bencana tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(height: 16.0), // Jarak antara textfield dan textfield berikutnya
+
+              // Gambar
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  // Tombol pilih gambar dari kamera
+                  TextButton(
+                    onPressed: () async {
+                      // Pilih gambar dari kamera
+                      final ImageSource source = ImageSource.camera;
+
+                      // Unggah gambar ke server
+                      final XFile? image =
+                          await ImagePicker().pickImage(source: source);
+
+                      if (image != null) {
+                        setState(() {
+                          _image = File(image.path);
+                        });
+                      }
+                    },
+                    child: Text("Pilih Gambar dari Kamera"),
+                  ),
+                ],
+              ),
+              _image != null
+                  ? Image.file(
+                      _image!,
+                      width: 200,
+                      height: 200,
+                    )
+                  : Container(),
+              SizedBox(height: 16.0), // Jarak antara gambar dan tombol Lapor
+
+              // Tombol Lapor
+              ElevatedButton(
+                onPressed: () {
+                  if (_validateForm()) {
+                    _submitReport();
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue, // Use backgroundColor instead of primary
+                  padding: EdgeInsets.all(16.0), // Perbesar tombol
+                ),
+                child: Text(
+                  'Lapor',
+                  style: TextStyle(
+                    color: Colors.white, // Warna teks tombol
+                    fontSize: 18, // Perbesar teks tombol
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
