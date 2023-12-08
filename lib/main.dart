23500fe46e236fc06cc65d9d1af833715614ea8f
BeepBob0411 <albertsimanullang12@gmail.com>
2023-12-08 14:44:56 +0700

    fix all

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:kren/screen/onboarding/screen_one.dart';
 import 'package:kren/screen/onboarding/screen_two.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
