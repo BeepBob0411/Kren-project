440191f656a9a9c2f222522aa93d940b88bb8882
BeepBob0411 <albertsimanullang12@gmail.com>
2023-12-09 09:55:21 +0700

    update

--- lib/officer/tampilan_petugas/nav_petugas.dart
+++ lib/officer/tampilan_petugas/nav_petugas.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:kren/screen/onboarding/screen_one.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'report_reception.dart'; // Import the ReportReception widget
 
 void main() {
   runApp(const NavPagesPetugas());
@@ -46,7 +45,7 @@ class NavPagesPetugas extends StatelessWidget {
       context,
       controller: PersistentTabController(initialIndex: 0),
       screens: _buildScreens(),
-      items: _navBarsItems(),
+      items: _navBarsItems(context),
       confineInSafeArea: true,
       backgroundColor: Colors.white,
       handleAndroidBackButtonPress: true,
@@ -83,7 +82,7 @@ class NavPagesPetugas extends StatelessWidget {
     ];
   }
 
-  List<PersistentBottomNavBarItem> _navBarsItems() {
+  List<PersistentBottomNavBarItem> _navBarsItems(BuildContext context) {
     return [
       PersistentBottomNavBarItem(
         icon: Icon(Icons.home),
@@ -127,131 +126,5 @@ class NavPagesPetugas extends StatelessWidget {
     }
 
     return isFirstTimeUser;
-  }
-}
-
-class ReportReception extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Laporan Diterima"),
-        ),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('reports').snapshots(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return Center(
-                child: Text("Error: ${snapshot.error}"),
-              );
-            }
-
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                DocumentSnapshot document = snapshot.data!.docs[index];
-                Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-                String imageUrl = data['imageUrl'] ?? '';
-                String lokasiBencana = data['lokasiBencana'] ?? '';
-
-                return Card(
-                  elevation: 3,
-                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: ListTile(
-                    title: Text("Jenis Bencana: ${data['jenisBencana']}"),
-                    subtitle: Text("Nama Bencana: ${data['namaBencana']}"),
-                    leading: imageUrl.isNotEmpty
-                        ? ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: CachedNetworkImage(
-                        imageUrl: imageUrl,
-                        width: 60,
-                        height: 60,
-                        fit: BoxFit.cover,
-                        placeholder: (context, url) => CircularProgressIndicator(),
-                        errorWidget: (context, url, error) => Icon(Icons.error),
-                      ),
-                    )
-                        : Container(
-                      width: 60,
-                      height: 60,
-                      color: Colors.grey[300],
-                      child: Icon(Icons.image, color: Colors.grey[600]),
-                    ),
-                    onTap: () {
-                      _showReportDetails(context, data, document.id, imageUrl, lokasiBencana);
-                    },
-                  ),
-                );
-              },
-            );
-          },
-        ),
-      ),
-    );
-  }
-
-  void _showReportDetails(BuildContext context, Map<String, dynamic> data, String documentId, String imageUrl, String lokasiBencana) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("Detail Laporan"),
-          content: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text("Jenis Bencana: ${data['jenisBencana']}"),
-              Text("Nama Bencana: ${data['namaBencana']}"),
-              Text("Lokasi Bencana: $lokasiBencana"),
-              Text("Keterangan Bencana: ${data['keteranganBencana']}"),
-              SizedBox(height: 16),
-              imageUrl.isNotEmpty
-                  ? Hero(
-                tag: imageUrl, // Use a unique tag for Hero animation
-                child: CachedNetworkImage(
-                  imageUrl: imageUrl,
-                  width: double.infinity,
-                  height: 200,
-                  fit: BoxFit.cover,
-                  placeholder: (context, url) => CircularProgressIndicator(),
-                  errorWidget: (context, url, error) {
-                    print("Error loading image: $error"); // Debugging print
-                    return Icon(Icons.error);
-                  },
-                ),
-              )
-                  : Container(),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("Tutup"),
-            ),
-            TextButton(
-              onPressed: () {
-                _deleteReport(documentId);
-                Navigator.pop(context);
-              },
-              child: Text("Hapus"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _deleteReport(String documentId) {
-    FirebaseFirestore.instance.collection('reports').doc(documentId).delete();
   }
 }
--- lib/officer/tampilan_petugas/report_reception.dart
+++ lib/officer/tampilan_petugas/report_reception.dart
@@ -1,56 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ReportReception extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Laporan Diterima"),
-        ),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('reports').snapshots(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return Center(
-                child: Text("Error: ${snapshot.error}"),
-              );
-            }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Laporan Diterima"),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('reports').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Error: ${snapshot.error}"),
+            );
+          }
 
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
 
-            return ListView(
-              children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-                String imageUrl = data['imageUrl'] ?? '';
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot document = snapshot.data!.docs[index];
+              Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+              String imageUrl = data['imageUrl'] ?? '';
 
-                return ListTile(
+              return Card(
+                elevation: 3,
+                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: ListTile(
                   title: Text("Jenis Bencana: ${data['jenisBencana']}"),
                   subtitle: Text("Nama Bencana: ${data['namaBencana']}"),
-                  leading: imageUrl.isNotEmpty
-                      ? Hero(
-                    tag: 'imageHero_$imageUrl', // Tag harus unik
-                    child: CachedNetworkImage(
-                      imageUrl: imageUrl,
-                      placeholder: (context, url) => CircularProgressIndicator(),
-                      errorWidget: (context, url, error) => Icon(Icons.error),
+                  leading: Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      image: DecorationImage(
+                        image: imageUrl.isNotEmpty
+                            ? CachedNetworkImageProvider(imageUrl)
+                            : AssetImage('assets/placeholder_image.png') as ImageProvider,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  )
-                      : Container(width: 50, height: 50), // Placeholder if no image
+                  ),
                   onTap: () {
                     _showReportDetails(context, data, document.id, imageUrl);
                   },
-                );
-              }).toList(),
-            );
-          },
-        ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
@@ -63,20 +71,51 @@ class ReportReception extends StatelessWidget {
           title: Text("Detail Laporan"),
           content: Container(
             width: MediaQuery.of(context).size.width * 0.8,
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.white,
+            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Jenis Bencana: ${data['jenisBencana']}"),
+                Text(
+                  "Jenis Bencana: ${data['jenisBencana']}",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 12),
                 Text("Nama Bencana: ${data['namaBencana']}"),
-                Text("Lokasi Bencana: ${data['lokasiBencana'] ?? ''}"),
+                SizedBox(height: 12),
+                GestureDetector(
+                  onTap: () {
+                    if (data['latitude'] != null && data['longitude'] != null) {
+                      _openGoogleMaps(data['latitude']?.toDouble(), data['longitude']?.toDouble());
+                    }
+                  },
+                  child: Text(
+                    "Lokasi Bencana: ${data['lokasiBencana'] ?? ''} (Klik untuk buka Google Maps)",
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 12),
                 Text("Keterangan Bencana: ${data['keteranganBencana'] ?? ''}"),
+                SizedBox(height: 24),
                 imageUrl.isNotEmpty
                     ? Hero(
-                  tag: 'imageHero_$imageUrl', // Tag harus unik
+                  tag: 'imageHero_$imageUrl',
                   child: CachedNetworkImage(
                     imageUrl: imageUrl,
                     placeholder: (context, url) => CircularProgressIndicator(),
                     errorWidget: (context, url, error) => Icon(Icons.error),
+                    width: double.infinity,
+                    height: 200,
+                    fit: BoxFit.cover,
+                    alignment: Alignment.center,
                   ),
                 )
                     : Container(),
@@ -84,18 +123,25 @@ class ReportReception extends StatelessWidget {
             ),
           ),
           actions: [
+            ElevatedButton(
+              onPressed: () {
+                _approveReport(documentId);
+                Navigator.pop(context);
+              },
+              child: Text("Approve"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _rejectReport(documentId);
+                Navigator.pop(context);
+              },
+              child: Text("Reject"),
+            ),
             TextButton(
               onPressed: () {
                 Navigator.pop(context);
               },
               child: Text("Tutup"),
-            ),
-            TextButton(
-              onPressed: () {
-                _deleteReport(documentId);
-                Navigator.pop(context);
-              },
-              child: Text("Hapus"),
             ),
           ],
         );
@@ -103,7 +149,23 @@ class ReportReception extends StatelessWidget {
     );
   }
 
-  void _deleteReport(String documentId) {
+  void _approveReport(String documentId) {
+    // Implement logic for approving a report
+  }
+
+  void _rejectReport(String documentId) {
+    // Implement logic for rejecting a report
     FirebaseFirestore.instance.collection('reports').doc(documentId).delete();
+  }
+
+  void _openGoogleMaps(double? latitude, double? longitude) async {
+    if (latitude != null && longitude != null) {
+      String url = "https://www.google.com/maps/search/?api=1&query=$latitude,$longitude";
+      if (await canLaunch(url)) {
+        await launch(url);
+      } else {
+        print("Could not launch Google Maps");
+      }
+    }
   }
 }
